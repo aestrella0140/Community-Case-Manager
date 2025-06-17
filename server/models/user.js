693476c2f90ec81d8361166126f7748e01e3364c
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2025-06-16 17:32:40 -0700

    User schema setup using bcrypt with saltRounds and bcrypt. compare to check if password is correct

---
+++ server/models/user.js
@@ -0,0 +1,41 @@
+const { Schema, model, default: mongoose } = require("mongoose");
+const bcrypt = require("bcrypt");
+
+const UserSchema = new Schema({
+  firstName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  lastName: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  password: {
+    type: String,
+    required: true,
+    trim: true,
+  },
+});
+
+UserSchema.pre("save", async function (next) {
+  if (this.isNew || this.isModified("password")) {
+    const saltRounds = 10;
+    this.password = await bcrypt.hash(this.password, saltRounds);
+  }
+
+  next();
+})
+
+UserSchema.methods.isCorrectPassword = async function (password) {
+    return bcrypt.compare(password, this.password);
+};
+
+const User = model('User', UserSchema);
+
+module.exports = User;
