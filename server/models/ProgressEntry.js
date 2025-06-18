667551db3fb18216d2927e85bcab45ec72e7256b
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2025-06-17 17:18:57 -0700

    setup for ProgressEntry model to separate note to handle general comments and ProgressEntry to handle structured updates

---
+++ server/models/ProgressEntry.js
@@ -0,0 +1,34 @@
+const { Schema, model } = require("mongoose");
+
+const progressEntrySchema = new Schema({
+    title: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    description: {
+        type: String,
+        required: true,
+        trim: true,
+    },
+    date: {
+        type: Date,
+        required: true,
+    },
+    status: {
+        type: String,
+        enum: ["completed", "missed", "in progress"]
+    },
+    case: {
+        type: Schema.Types.ObjectId,
+        ref: "Case",
+    },
+    createdBy: {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+    },
+})
+
+const ProgressEntry = model("ProgressEntry", progressEntrySchema);
+
+module.exports = ProgressEntry;
--- server/models/index.js
+++ server/models/index.js
@@ -1,5 +1,6 @@
 const User = require('./User');
 const Note = require('./Note');
 const Case = require('./Case');
+const ProgressEntry = require('./ProgressEntry');
 
-module.exports = { User, Note, Case };
+module.exports = { User, Note, Case, ProgressEntry };
