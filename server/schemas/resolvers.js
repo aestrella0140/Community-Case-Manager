12507159958ee1421105081fdc39afd72335df23
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2025-07-09 17:39:55 -0700

    added mutations for case note and user pushing case into user and note into case

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,51 +1,104 @@
-const { User, Case, Note, ProgressEntry, } = require('../models');
+const { User, Case, Note, ProgressEntry } = require("../models");
 
-const { signToken, AuthenticationError } = require('../utils/auth');
+const { signToken, AuthenticationError } = require("../utils/auth");
 
 const resolvers = {
-    Query: {
-        Users: async () => {
-            return User.find();
-        },
-
-        user: async (parent, { userId }) => {
-            return User.findOne({ _id: userId });
-        },
-
-        Cases: async () => {
-            return Case.find();
-        },
-
-        case: async (parent, { caseId }) => {
-            return Case.findOne({ _id: caseId });
-        },
-
-        notes: async (userId) => {
-            return Note.find({ _id: userId});
-        },
+  Query: {
+    Users: async () => {
+      return User.find();
     },
 
-    Mutation: {
-        addUser: async (parent, { userId }) => {
-            const user = await User.create({firstName, lastName, email, password});
-            const token = signToken(user);
-        },
+    user: async (parent, { userId }) => {
+      return User.findOne({ _id: userId });
+    },
 
-        login: async(parent, { email, password }) => {
-            const user = await User.findOne({ email });
+    Cases: async () => {
+      return Case.find();
+    },
 
-            if (!user) {
-                throw AuthenticationError;
-            }
+    case: async (parent, { caseId }) => {
+      return Case.findOne({ _id: caseId });
+    },
 
-            const correctPw = await user.isCorrectPassword(password);
+    notes: async (parent, { userId }) => {
+      return Note.find({ author: userId });
+    },
+  },
 
-            if (!correctPw) {
-                throw AuthenticationError
-            }
+  Mutation: {
+    addUser: async (parent, { firstName, lastName, email, password, role }) => {
+      const user = await User.create({
+        firstName,
+        lastName,
+        email,
+        password,
+        role,
+      });
+      const token = signToken(user);
+      return { token, user };
+    },
 
-            const token = signToken(user);
-            return { token, user };
-        },
-    }
-}
+    login: async (parent, { email, password }) => {
+      const user = await User.findOne({ email });
+
+      if (!user) {
+        throw AuthenticationError;
+      }
+
+      const correctPw = await user.isCorrectPassword(password);
+
+      if (!correctPw) {
+        throw AuthenticationError;
+      }
+
+      const token = signToken(user);
+      return { token, user };
+    },
+
+    addCase: async (
+      parent,
+      { firstName, lastName, dob, assignedTo, status }
+    ) => {
+      const newCase = await Case.create({
+        firstName,
+        lastName,
+        dob,
+        assignedTo,
+        status,
+      });
+
+      awaitUser.findByIdAndUpdate(assignedTo, {
+        $push: { cases: newCase._id },
+      });
+      return newCase;
+    },
+
+    addNote: async (parent, { content, authorId, caseId }) => {
+      const note = await Note.create({
+        content,
+        author: authorId,
+        case: caseId,
+      });
+
+      await Case.findByIdAndUpdate(caseId, {
+        $push: { notes: note._id },
+      });
+
+      return note;
+    },
+
+    addProgressEntry: async (
+      parent,
+      { title, description, date, status, case: caseId, createdBy }
+    ) => {
+      const entry = await ProgressEntry.create({
+        title,
+        description,
+        date,
+        status,
+        case: caseId,
+        createdBy,
+      });
+    },
+  },
+};
