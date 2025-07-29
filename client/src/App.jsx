ee21cd9a7b345f06b3cca964c848f6183871ba1c
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2025-07-28 17:00:06 -0700

    finished graphql setup for server to client launch with localhost using apolloProvider

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,44 @@
-import { useState } from 'react'
+import { Outlet } from 'react-router-dom';
 
-import './App.css'
+import {
+  ApolloClient,
+  InMemoryCache,
+  ApolloProvider,
+  createHttpLink,
+} from "@apollo/client";
+
+import { setContext } from "@apollo/client/link/context";
+
+
+import './App.css';
+
+const httpLink = createHttpLink({
+  uri: import.meta.env.VITE_GRAPHQL_URI,
+});
+
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem('id_token');
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `bearer${token}` : '',
+    },
+  };
+});
+
+const client = new ApolloClient({
+  link: authLink.concat(httpLink),
+  cache: new InMemoryCache(),
+});
 
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
-    <div>
-      
-    </div>
+    <ApolloProvider>
+      <Outlet />
+    </ApolloProvider>
   )
 }
 
-export default App
+export default App;
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import ReactDOM from "react-dom/client";
+import React from "react";
+import "./index.css";
+import App from "./App.jsx";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+
+      }
+    ]
+  }
+])
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+ <RouterProvider router={router} />
+);
