a64aff26c818023591160d8f0cfd4d23319de15b
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2025-08-13 16:44:07 -0700

    auth.js boilerplate for handling AuthServices needed for front end functions

--- client/src/components/nav.jsx
+++ client/src/components/nav.jsx
@@ -2,6 +2,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
+
 function Nav() {
   return (
     <nav className="relative flex w-full flex-wrap items-center justify-between bg-zinc-50 py-2 shadow-dark-mild dark:bg-neutral-700 lg:py-4">
