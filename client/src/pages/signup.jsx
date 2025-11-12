c48af06b998ff69c9626a941b4463fb32bd72483
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2025-11-11 17:29:31 -0700

    some styles for signup page for layout

--- client/src/pages/signup.jsx
+++ client/src/pages/signup.jsx
@@ -95,7 +95,7 @@ const Signup = () => {
                 ethnicity: values.ethnicity,
                 ethnicityOther: values.ethnicityOther,
                 role: values.role,
-              }
+              },
             },
           })
             .then(({ data, errors }) => {
@@ -104,7 +104,7 @@ const Signup = () => {
               }
               setSubmitMessage("User created Successfully.");
               SetShowMessage(true);
-              auth.login(data.addUser.token)
+              auth.login(data.addUser.token);
             })
             .catch((error) => {
               console.error("Mutation Error:", error);
@@ -119,100 +119,135 @@ const Signup = () => {
         {({ values, handleChange, isSubmitting }) => {
           return (
             <>
-              <Form>
-                <div>
-                  <label htmlFor="firstName">First Name:</label>
-                  <Field
+              <div className="flex justify-center items-center min-h-screen bg-gray-100">
+                <Form className="flex flex-col items-start">
+                  <div className="flex flex-col w-full max-w-sm mb-4">
+                    <label htmlFor="firstName" className="text-sm font-medium text-gray-700 mb-1">First Name:</label>
+                    <Field
+                      type="text"
+                      name="firstName"
+                      id="firstName"
+                      className="border border-gray-300 rounded px-2 py-1 w-full"
+                    />
+                    <ErrorMessage
+                      name="firstName"
+                      component="div"
+                      className="border border-gray-300 rounded px-2 py-1 w-full"
+                    />
+                  </div>
+
+                  <div className="flex flex-col w-full max-w-sm mb-4">
+                    <label htmlFor="lastName" >Last Name:</label>
+                    <Field
+                      type="text"
+                      name="lastName"
+                      id="lastName"
+                      className="border border-gray-300 rounded px-2 py-1 w-full"
+                    />
+                    <ErrorMessage
+                      name="lastName"
+                      component="div"
+                      className="border border-gray-300 rounded px-2 py-1 w-full"
+                    />
+                  </div>
+
+                  <div className="flex flex-col w-full max-w-sm mb-4">
+                    <label htmlFor="email">Email:</label>
+                    <Field 
+                    type="text" 
+                    name="email" 
+                    id="email" 
+                    className="border border-gray-300 rounded px-2 py-1 w-full" 
+                    />
+                    <ErrorMessage 
+                    name="email" 
+                    component="div" 
+                    className="" 
+                    />
+                  </div>
+
+                  <div className="flex flex-col w-full max-w-sm mb-4">
+                    <label htmlFor="password">Password:</label>
+                    <Field 
                     type="text"
-                    name="firstName"
-                    id="firstName"
-                    className=""
-                  />
-                  <ErrorMessage name="firstName" component="div" className="" />
-                </div>
+                    name="password" 
+                    id="pwd" 
+                    className="border border-gray-300 rounded px-2 py-1 w-full" />
+                    <ErrorMessage
+                      name="password"
+                      component="div"
+                      className="border border-gray-300 rounded px-2 py-1 w-full"
+                    />
+                  </div>
 
-                <div>
-                  <label htmlFor="lastName">Last Name:</label>
-                  <Field
-                    type="text"
-                    name="lastName"
-                    id="lastName"
-                    className=""
-                  />
-                  <ErrorMessage name="lastName" component="div" className="" />
-                </div>
+                  <div className="flex flex-col w-full max-w-sm mb-4">
+                    <label htmlFor="gender">Gender:</label>
+                    <Field
+                      as="select"
+                      name="gender"
+                      id="gender"
+                      className="border border-gray-300 rounded px-2 py-1 w-full"
+                      onChange={(e) => {
+                        handleChange(e);
+                        if (e.target.value === "Other") {
+                          setShowGenderModal(true);
+                        }
+                      }}
+                    >
+                      <option value="">Select Gender</option>
+                      {genderOptions.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </Field>
+                    <ErrorMessage name="gender" component="div" className="" />
+                  </div>
 
-                <div>
-                  <label htmlFor="email">Email:</label>
-                  <Field type="text" name="email" id="email" className="" />
-                  <ErrorMessage name="email" component="div" className="" />
-                </div>
+                  <div className="flex flex-col w-full max-w-sm mb-4">
+                    <label htmlFor="ethnicity">Ethnicity:</label>
+                    <Field
+                      as="select"
+                      name="ethnicity"
+                      id="ethnicity"
+                      className="border border-gray-300 rounded px-2 py-1 w-full"
+                      onChange={(e) => {
+                        handleChange(e);
+                        if (e.target.value === "Other") {
+                          setShowEthnicityModal(true);
+                        }
+                      }}
+                    >
+                      <option value="">Select Ethnicity</option>
+                      {ethnicityOptions.map((option) => (
+                        <option key={option} value={option}>
+                          {option}
+                        </option>
+                      ))}
+                    </Field>
+                    <ErrorMessage
+                      name="ethnicity"
+                      component="div"
+                      className=""
+                    />
+                  </div>
 
-                <div>
-                  <label htmlFor="password">Password:</label>
-                  <Field type="text" name="password" id="pwd" className="" />
-                  <ErrorMessage name="password" component="div" className="" />
-                </div>
+                  <div className="flex flex-col w-full max-w-sm mb-4">
+                    <label htmlFor="role">Role:</label>
+                    <Field 
+                    type="text" 
+                    name="role" 
+                    id="role" 
+                    className="border border-gray-300 rounded px-2 py-1 w-full" 
+                    />
+                    <ErrorMessage name="role" component="div" className="" />
+                  </div>
 
-              
-                <div>
-                  <label htmlFor="gender">Gender:</label>
-                  <Field
-                    as="select"
-                    name="gender"
-                    id="gender"
-                    onChange={(e) => {
-                      handleChange(e);
-                      if (e.target.value === "Other") {
-                        setShowGenderModal(true);
-                      }
-                    }}
-                  >
-                    <option value="">Select Gender</option>
-                    {genderOptions.map((option) => (
-                      <option key={option} value={option}>
-                        {option}
-                      </option>
-                    ))}
-                  </Field>
-                  <ErrorMessage name="gender" component="div" className="" />
-                </div>
-
-                
-
-                <div>
-                  <label htmlFor="ethnicity">Ethnicity:</label>
-                  <Field
-                    as="select"
-                    name="ethnicity"
-                    id="ethnicity"
-                    onChange={(e) => {
-                      handleChange(e);
-                      if (e.target.value === "Other") {
-                        setShowEthnicityModal(true);
-                      }
-                    }}
-                  >
-                    <option value="">Select Ethnicity</option>
-                    {ethnicityOptions.map((option) => (
-                      <option key={option} value={option}>
-                        {option}
-                      </option>
-                    ))}
-                  </Field>
-                  <ErrorMessage name="ethnicity" component="div" className="" />
-                </div>
-
-                <div>
-                  <label htmlFor="role">Role:</label>
-                  <Field type="text" name="role" id="role" className="" />
-                  <ErrorMessage name="role" component="div" className="" />
-                </div>
-
-                <button type="submit" disabled={isSubmitting}>
-                  Submit
-                </button>
-              </Form>
+                  <button type="submit" disabled={isSubmitting}>
+                    Submit
+                  </button>
+                </Form>
+              </div>
 
               {showGenderModal && (
                 <Modal
