d677c49416e33ea3350bf1ee4447d44751437c71
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2026-02-04 17:02:26 -0700

    added initial setup for the header on the dashboard page using GET_USER_BY_ID query

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -2,15 +2,23 @@ import React from "react";
 import { useMutation, useQuery } from "@apollo/client";
 import auth from "../utils/auth";
 
+import { GET_USER_BY_ID, GET_CASE_BY_ID } from "../utils/queries";
 
+const Dashboard = ({userId}) => {
+const {loading, error, data } = useQuery(GET_USER_BY_ID, {
+    variables: { id: userId },
+});
 
-const Dashboard = () => {
+if (loading || error) return <p>loading data</p>
+
+const { firstName, lastName } = data.getUserById
+
     return (
-        <div>
-            <header>
-
-            </header>
-        </div>
+        <header>
+            <h1>
+                {firstName}{lastName}
+            </h1>
+        </header>
     )
 };
 
