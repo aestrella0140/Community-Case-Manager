8ee44f639be6c24f37570e2150ad2134417b902b
aestrella0140 <133534749+aestrella0140@users.noreply.github.com>
2026-02-17 17:19:12 -0700

    added date time implementation for dashboard greeting header using dayjs

--- client/src/pages/dashboard.jsx
+++ client/src/pages/dashboard.jsx
@@ -1,44 +1,54 @@
-import React from "react";
+import React, { useState } from "react";
 import { useMutation, useQuery } from "@apollo/client";
 import auth from "../utils/auth";
 import { useNavigate } from "react-router-dom";
+import dayjs from 'dayjs';
 
 import { GET_USER_BY_ID, GET_CASE_BY_ID } from "../utils/queries";
 
 const Dashboard = () => {
-    const user = auth.getUser();
-    const userId = user?.data?._id;
+    const [currentDate, setCurrentDate] = useState(dayjs());
+    
+    const formattedDate = currentDate.format('DD MMMM YYYY');
 
-const {loading, error, data } = useQuery(GET_USER_BY_ID, {
+  const user = auth.getUser();
+  const userId = user?.data?._id;
+
+  const { loading, error, data } = useQuery(GET_USER_BY_ID, {
     variables: { getUserByIdId: userId },
     skip: !userId,
-});
+  });
 
-const navigate = useNavigate();
+  const navigate = useNavigate();
 
-const handleLogout = () => {
+  const handleLogout = () => {
     localStorage.removeItem("id_token");
     navigate("/login");
-};
+  };
 
-if (loading) return <p>loading</p>
+  if (loading) return <p>loading</p>;
 
-if (error) {
+  if (error) {
     console.error(error);
-    return <p>Error loading</p>
-}
+    return <p>Error loading</p>;
+  }
 
+  const { firstName, lastName } = data.getUserById;
 
-const { firstName, lastName } = data.getUserById;
+  return (
+    <>
+      <header>
+        <h1>Welcome back, {firstName} {lastName} {formattedDate}</h1>
+        <button onClick={handleLogout}>logout</button>
+      </header>
 
-    return (
-        <header>
-            <h1>
-                {firstName} {lastName}
-            </h1>
-            <button onClick={handleLogout}>logout</button>
-        </header>
-    )
+      <main>
+        <section>
+            <h2>Dashboard overview</h2>
+        </section>
+      </main>
+    </>
+  );
 };
 
 export default Dashboard;
